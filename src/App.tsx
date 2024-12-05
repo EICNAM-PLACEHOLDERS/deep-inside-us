0e93e7651606569fb61723e7c3ae94cebd8d8669
lege0053 <marine.leg02@gmail.com>
2024-12-05 23:51:06 +0100

    feat: wave parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import WaveParallax from "./WaveParallx";
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <WaveParallax />
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est neque ducimus aspernatur voluptas consequatur tempora rem similique expedita at odit. Ratione veniam vero maxime! Totam sed unde distinctio iusto a.</h1>
+    </div>
+  );
 }
