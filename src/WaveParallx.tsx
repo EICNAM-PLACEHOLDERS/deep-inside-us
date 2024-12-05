34526443285be0028eb810cf424aa50c0db5bbe1
lege0053 <marine.leg02@gmail.com>
2024-12-06 00:22:22 +0100

    feat: boat

--- src/WaveParallx.tsx
+++ src/WaveParallx.tsx
@@ -7,6 +7,7 @@ const WaveParallax: React.FC = () => {
     const wave2 = document.getElementById("wave2")!;
     const wave3 = document.getElementById("wave3")!;
     const wave4 = document.getElementById("wave4")!;
+    const boat = document.getElementById("boat")!;
 
     const handleScroll = () => {
       const value = window.scrollY;
@@ -14,6 +15,8 @@ const WaveParallax: React.FC = () => {
       wave2.style.backgroundPositionX = 300 + value * -4 + "px";
       wave3.style.backgroundPositionX = 200 + value * 2 + "px";
       wave4.style.backgroundPositionX = 100 + value * -2 + "px";
+
+      boat.style.transform = `translateX(-50%) translateY(${value * 0.2}px)`;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -26,11 +29,10 @@ const WaveParallax: React.FC = () => {
       <div className="wave" id="wave2" style={{ "--i": 2 } as React.CSSProperties}></div>
       <div className="wave" id="wave3" style={{ "--i": 3 } as React.CSSProperties}></div>
       <div className="wave" id="wave4" style={{ "--i": 4 } as React.CSSProperties}></div>
+      <div className="boat" id="boat" ></div>
       <div className="sec">
         <h2>Deep Inside Us</h2>
-        <p>
-            Plonger, c’est redécouvrir à quel point nous faisons partie d’un tout. L’océan est le miroir de notre propre corps. Prenons soin des deux.
-        </p>
+        <p>Plonger, c’est redécouvrir à quel point nous faisons partie d’un tout. L’océan est le miroir de notre propre corps. Prenons soin des deux.</p>
       </div>
     </section>
   );
