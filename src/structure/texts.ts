7bc10d1f1983d07fd829c3dbe638a2d1b346ae61
lege0053 <marine.leg02@gmail.com>
2024-12-06 06:51:05 +0100

    fix: text

--- src/structure/texts.ts
+++ src/structure/texts.ts
@@ -1,16 +1,12 @@
 export const depthTexts = [
     { depth: 0, text: "🌊🤿 Plonger dans l’océan, c’est comme explorer une version 🌌 de soi-même. Prêt à découvrir ce que nous avons en commun ? 🤔✨ Allons-y ! 🐟🐚" },
-    { depth: 1, text: "🌊✨ La peau de l’océan, c’est sa surface. Elle échange 🌬️, régule l’énergie ☀️... comme notre peau 👩‍🦰👨‍🦱 régule 🌡️ et nous protège 🛡️." },
-    { depth: 10, text: "🌊🌪️ Ces courants marins... ce sont les veines et les artères 🩸 de l’océan. Comme notre système circulatoire ❤️, ils transportent 🛠️ les nutriments 🍎 et régulent 🌡️ la température 🌍." },
-    { depth: 20, text: "🌱🐟 Et ce phytoplancton ? Ce sont les poumons 🌬️ de l’océan, produisant de l’oxygène 🌿 et absorbant le CO₂ 🏭. Tout comme nos poumons 🫁, ils maintiennent l’équilibre ⚖️." },
-    { depth: 30, text: "🐠🪸 Regardez ces récifs coralliens : la charpente 🏗️ de la vie marine, tout comme notre squelette 🦴 soutient et protège notre corps. 🌍🌊 Sans eux, la biodiversité 🌱🦈 s’effondrerait." },
-    { depth: 50, text: "💧🌊 Saviez-vous que l’eau salée 🧂 de l’océan et le plasma sanguin 🩸 partagent presque la même composition chimique 🧪 ? L’un maintient la vie marine 🐟, l’autre équilibre nos cellules 🧬." },
-    { depth: 100, text: "🌡️🌊 La température de l’eau, comme celle de notre corps 🫀, doit rester stable pour préserver l’équilibre ⚖️. Homéostasie thermique 🌀, dans l’océan ou dans nos veines 🩸, c’est pareil ! 🥶🥵" },
-    { depth: 150, text: "🌊🏞️ Ces rivières sous-marines 🚰 transportent des sédiments et des nutriments, tout comme nos vaisseaux sanguins 🩸🚛 déplacent le sang, les nutriments 🍊 et l’oxygène 🌬️ à travers notre corps. 🌟 Un système parfait." },
-    { depth: 200, text: "🛡️🌊 L’océan protège la vie 🐟, tout comme notre système immunitaire 🧬 veille sur nous. Chaque élément 🌱 a son rôle : défense ⚔️, régulation 🌀, protection 🛡️." },
-    { depth: 250, text: "💓🌊 La pompe thermohaline 🌊⛴️, c’est le cœur ❤️ de l’océan. Elle distribue chaleur 🔥 et nutriments 🧪, tout comme notre cœur 🫀 fait battre la vie en nous. 🌍💙 Deux systèmes, un même souffle." },
-    { depth: 3800, text: "🌌⚓ À 3800 mètres de profondeur, dans le silence absolu 🤫🌊, l'épave du Titanic 🚢 demeure. Un vestige du passé 📜, un monde englouti 🌌, et pourtant... une partie de nous. 💔" }
+    { depth: 20, text: "🌊✨ La peau de l’océan, c’est sa surface. Elle échange 🌬️, régule l’énergie ☀️... comme notre peau 👩‍🦰👨‍🦱 régule 🌡️ et nous protège 🛡️." },
+    { depth: 60, text: "🌊🌪️ Ces courants marins... ce sont les veines et les artères 🩸 de l’océan. Comme notre système circulatoire ❤️, ils transportent 🛠️ les nutriments 🍎 et régulent 🌡️ la température 🌍." },
+    { depth: 100, text: "🌱🐟 Et ce phytoplancton ? Ce sont les poumons 🌬️ de l’océan, produisant de l’oxygène 🌿 et absorbant le CO₂ 🏭. Tout comme nos poumons, ils maintiennent l’équilibre ⚖️." },
+    { depth: 110, text: "🐠 Regardez ces récifs coralliens : la charpente 🏗️ de la vie marine, tout comme notre squelette 🦴 soutient et protège notre corps. 🌍🌊 Sans eux, la biodiversité 🌱🦈 s’effondrerait." },
+    { depth: 130, text: "💧🌊 Saviez-vous que l’eau salée 🧂 de l’océan et le plasma sanguin 🩸 partagent presque la même composition chimique 🧪 ? L’un maintient la vie marine 🐟, l’autre équilibre nos cellules 🧬." },
+    { depth: 150, text: "🌡️🌊 La température de l’eau, comme celle de notre corps, doit rester stable pour préserver l’équilibre ⚖️. Homéostasie thermique 🌀, dans l’océan ou dans nos veines 🩸, c’est pareil ! 🥶🥵" },
+    { depth: 170, text: "🌊🏞️ Ces rivières sous-marines 🚰 transportent des sédiments et des nutriments, tout comme nos vaisseaux sanguins 🩸🚛 déplacent le sang, les nutriments 🍊 et l’oxygène 🌬️ à travers notre corps. 🌟 Un système parfait." },
+    { depth: 190, text: "🛡️🌊 L’océan protège la vie 🐟, tout comme notre système immunitaire 🧬 veille sur nous. Chaque élément 🌱 a son rôle : défense ⚔️, régulation 🌀, protection 🛡️." },
+    { depth: 210, text: "💓🌊 La pompe thermohaline 🌊⛴️, c’est le cœur ❤️ de l’océan. Elle distribue chaleur 🔥 et nutriments 🧪, tout comme notre cœur  fait battre la vie en nous. 🌍💙 Deux systèmes, un même souffle." },
 ];
-
-
-  
