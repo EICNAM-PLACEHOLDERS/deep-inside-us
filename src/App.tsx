eafd969910403ebf889360b585f3048717fd9ff7
Lilierd 42 <lilierdyt@gmail.com>
2024-12-06 06:16:22 +0100

    Ergo finie

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import WaveParallax from "./WaveParallax";
 import collineImage from './assets/colline.png';
+import Ergo from "./Ergo";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WaveParallax />}>
+        </Route>
+        <Route path="/ergonomie" element={<Ergo />}>
         </Route>
       </Routes>
     </BrowserRouter>
